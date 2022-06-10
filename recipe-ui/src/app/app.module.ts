a71dd5ab48c1d26c9bf6ad7bf1925fbdd9fb3e55
Hassan <hassansaleem194@gmail.com>
2022-06-10 17:22:27 +0100

    set up Angular project, cleaned up template, added routing for recipe list page

---
+++ recipe-ui/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecipeListComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
